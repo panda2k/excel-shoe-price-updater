0695fb72d4446b0420fa2c8490ce22d4e900517f
panda2k <thelowkeypanda@gmail.com>
2021-04-05 20:05:40 -0700

    basic functionality

--- app.ts
+++ app.ts
@@ -1,51 +1,73 @@
 import xlsx = require('xlsx')
+import scraper = require('./scrapers/goat')
 
 import settings from './settings.json'
 
-console.log(settings.sheetLocation)
-
 const sheet = xlsx.readFile(settings.sheetLocation)
 
-const sheetNames = sheet.Props?.SheetNames
+const sheetNames = sheet.Props?.SheetNames;
 
-if (sheetNames) {
-    for (let i = 0; i < sheetNames.length; i++) {
-        console.log(`Processhing Sheet ${sheetNames[i]}`)
-        let nameRow: string|undefined
-        let sizeRow: string|undefined
-        let valueRow: string|undefined
-        let payoutRow: string|undefined
+(async() => {
+    if (sheetNames) {
+        for (let i = 0; i < sheetNames.length; i++) {
+            console.log(`Processhing Sheet ${sheetNames[i]}`)
+            let nameRow: string|undefined
+            let sizeRow: string|undefined
+            let valueRow: string|undefined
+            let payoutRow: string|undefined
+    
+            for (const [key, value] of Object.entries(sheet.Sheets[sheetNames[i]])) {
+                if (value.v == 'Shoe Name') {
+                    nameRow = key
+                } else if (value.v == 'Size') {
+                    sizeRow = key
+                } else if (value.v == 'Estimated Value') {
+                    valueRow = key
+                } else if (value.v == 'Payout') {
+                    payoutRow = key
+                }
+            }
+    
+            if (!nameRow) {
+                console.log("Missing shoe name row. Make sure it is titled 'Shoe Name'")
+                break
+            } else if (!sizeRow) {
+                console.log("Missing shoe size row. Make sure it is named 'Size'")
+                break
+            } else if (!valueRow) {
+                console.log("Missing estimated value row. Make sure it is named 'Estimated Value'")
+                break
+            } else if (!payoutRow) {
+                console.log("Missing payout row. Make sure it is named 'Payout'")
+                break
+            }
+    
+            let currentRow: number
+            const nameColumn = nameRow.replace(/[0-9]/, '')
+    
+            currentRow = parseInt(nameRow.replace(/[a-zA-Z]+/, '')) + 1
+            
+            const payoutColumn = payoutRow.replace(/[0-9]/, '')
+            const sizeColumn = sizeRow.replace(/[0-9]/, '')
+            const valueColumn = valueRow.replace(/[0-9]/, '')
 
-        for (const [key, value] of Object.entries(sheet.Sheets[sheetNames[0]])) {
-            if (value.v == 'Name') {
-                nameRow = key
-                console.log(`Name: ${nameRow}`)
-            } else if (value.v == 'Size') {
-                sizeRow = key
-                console.log(`Size: ${sizeRow}`)
-            } else if (value.v == 'Estimated Value') {
-                valueRow = key
-                console.log(`Value ${valueRow}`)
-            } else if (value.v == 'Payout') {
-                payoutRow = key
-                console.log(`Payout: ${payoutRow}`)
+            while (sheet.Sheets[sheetNames[i]][nameColumn + currentRow]) {
+                console.log(sheet.Sheets[sheetNames[i]][nameColumn + currentRow].v)
+    
+                if (!sheet.Sheets[sheetNames[i]][payoutColumn + currentRow]) { // shoe hasnt been sold
+                    const slug = (await scraper.searchShoes(sheet.Sheets[sheetNames[i]][nameColumn + currentRow].v)).slug
+                    const price = await scraper.getShoePrice(slug, parseInt(sheet.Sheets[sheetNames[i]][sizeColumn + currentRow].v))
+
+                    console.log(slug)
+                    console.log(price)
+                    sheet.Sheets[sheetNames[i]][valueColumn + currentRow].v = price / 100
+                }
+                currentRow++
             }
         }
-
-        if (!nameRow) {
-            console.log("Missing shoe name row. Make sure it is titled 'Name'")
-            break
-        } else if (!sizeRow) {
-            console.log("Missing shoe size row. Make sure it is named 'Size'")
-            break
-        } else if (!valueRow) {
-            console.log("Missing estimated value row. Make sure it is named 'Estimated Value'")
-            break
-        } else if (!payoutRow) {
-            console.log("Missing payout row. Make sure it is named 'Payout'")
-            break
-        }
-
-
+        xlsx.writeFile(sheet, settings.sheetLocation)
+        console.log(`Wrote file to ${settings.sheetLocation}`)
     }
-}
+})()
+
+
